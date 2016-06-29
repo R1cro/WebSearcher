2c210947ae0c7f8f2dd235fa55317c483d6ad699
Anatoly <teseyko@outlook.com>
2016-06-29 13:27:10 +0300

    Update WebSearcher.rb

--- WebSearcher.rb
+++ WebSearcher.rb
@@ -1,12 +1,10 @@
-require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
 require 'certified'
-#require 'pp'
 require 'open_uri_redirections'
-#require 'concurrent'
 require 'thread'
+#require 'pp'
 
 CSV_HEADER = %w(Link Count) 	
 
@@ -18,7 +16,6 @@ class WebSearcher
   	@term = term
   end
 
- # Show and count all terms on page.
  #  def frequencies(words) 
  #    Hash[
  #    	 words.group_by(&:downcase).map{ |word,instances|
@@ -27,55 +24,64 @@ class WebSearcher
  #    ]
  #  end
 
- def count_term(words, term)
-   words.chars.each_cons(term.size).map(&:join).count(term)
- end
+  def find_term(page)
+    html = Nokogiri::HTML(open(page, :allow_redirections => :all))
+    html.css('script').remove
+    preview = html.at('html').inner_text.scan(/\w+/)
+    words = html.at('html').inner_text
+    @count_term = count_term(words, term).to_s
+  end
 
- def save_to_csv
-   started_at = Time.now
-   CSV.open('results.txt', 'wb') do |csv_output|  
-     csv_output << CSV_HEADER 
-     csv_text = File.read('urls.csv')
-     csv_input = CSV.parse(csv_text, :headers => true)
-     work_q = Queue.new
-     csv_input.each { |row| work_q <<  'https://www.' + row[1]  }
-     workers = (0...5).map do
-       Thread.new do
-         begin 
-           while page = work_q.pop('true')
-             retries = 1
-             begin
-               html = Nokogiri::HTML(open(page, :allow_redirections => :all))
-               html.css('script').remove
-               preview = html.at('html').inner_text.downcase.scan(/\w+/)
-               words = html.at('html').inner_text.downcase
-               count_term = count_term(words, term.downcase).to_s
-               puts page + ': ' + count_term + ' found.' 
-               csv_output << [ page, count_term ] 
-             rescue StandardError=>e
-               puts "#{page}: #{e}"
-               if retries > 0
-                 puts "Retrying #{retries} more times"
-                 retries -= 1
-                 sleep 1
-                 retry
-               end
-               else
-              	 sleep 1.0 + rand * 1.0
-               end
-             end
-             rescue ThreadError
-           end
-         end
-       end
-       workers.map(&:join);
-       puts "Task made in " + (Time.now - started_at).inspect + ' seconds.'
-     end #workers
-   end #def 
+  def count_term(words, term)
+    words.chars.each_cons(term.size).map(&:join).count(term)
+  end
+
+  def read_csv
+    csv_text = File.read('urls.csv')
+    csv_input = CSV.parse(csv_text, :headers => true)
+  end
+
+  def save_to_result
+    started_at = Time.now
+    CSV.open('results.txt', 'wb') do |csv_output|  
+      csv_output << CSV_HEADER 
+      work_q = Queue.new
+      read_csv.each { |row| work_q <<  'https://www.' + row[1] }
+      workers = (0...26).map do
+        Thread.new do
+          begin 
+            while page = work_q.pop('true')
+              retries = 1
+              begin
+                find_term(page) 
+                puts page + ': ' + @count_term + ' found.' 
+                csv_output << [ page, @count_term ] 
+              rescue StandardError=>e
+                puts "#{page}: #{e}"
+                if retries > 0
+                  puts "Retrying #{retries} more times"
+                  retries -= 1
+                  sleep 3
+                  retry
+                end
+                else
+              	  sleep 2.0 + rand * 2
+                end
+              end
+          rescue ThreadError
+          end
+        end #begin_end
+      end #workers
+      workers.map(&:join);
+      puts "Task made in " + (Time.now - started_at).inspect + ' seconds.'
+    end #csv_output
+  end #def
+
 end #class
 
 web_searcher = WebSearcher.new(ARGV[0])
-web_searcher.save_to_csv
+web_searcher.save_to_result
+
 
 
 
